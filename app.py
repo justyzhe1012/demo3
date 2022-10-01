688bf10682481a7af11b8555dfa81a243e32473b
justyzhe1012 <114711290+justyzhe1012@users.noreply.github.com>
2022-10-01 23:56:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
-                        thumbnail_image_url='https://www.freepik.com/free-vector/young-businesswoman-showing-thumb-up-sign-cartoon-illustration_12898746.htm#query=manager%20cartoon&position=5&from_view=search',
+                        thumbnail_image_url='https://img.freepik.com/free-vector/young-businesswoman-showing-thumb-up-sign-cartoon-illustration_56104-520.jpg?w=900&t=st=1664639444~exp=1664640044~hmac=831314e1bd2e9b8cc67786af02e9c1f2b9969a7735bf553c296ca70841b92b32',
                         title='我是PM!',
                         text='PM雲端連結',
                         actions=[
@@ -61,7 +61,7 @@ def handle_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://www.freepik.com/free-vector/young-businesswoman-holding-tablet-thumb-up-cartoon-illustration_12898721.htm#query=businesswoman%20cartoon&position=1&from_view=search',
+                        thumbnail_image_url='https://img.freepik.com/free-vector/young-businesswoman-holding-smartphone-show-ok-sign-cartoon-illustration_56104-508.jpg?w=900&t=st=1664639472~exp=1664640072~hmac=b1e5632d2228ea715e83a0177ff3c31015ce7f93dcf32291c1dac47d08fd11c4',
                         title='我是學程部!',
                         text='學程部雲端連結',
                         actions=[
@@ -76,7 +76,7 @@ def handle_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://www.freepik.com/free-vector/young-businesswoman-holding-smartphone-show-ok-sign-cartoon-illustration_12898719.htm#query=businesswoman%20cartoon&position=2&from_view=search',
+                        thumbnail_image_url='https://img.freepik.com/free-vector/young-businesswoman-holding-tablet-thumb-up-cartoon-illustration_56104-509.jpg?w=900&t=st=1664639489~exp=1664640089~hmac=655fc9ff6d53a9c358d559ecf75cd448d4348e91eec3622473e1604991d53886',
                         title='我是課程部!',
                         text='課程部雲端連結',
                         actions=[
@@ -91,7 +91,7 @@ def handle_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        thumbnail_image_url='https://www.freepik.com/free-vector/young-businesswoman-holding-tablet-cartoon-illustration_12898722.htm#query=businesswoman%20cartoon&position=11&from_view=search',
+                        thumbnail_image_url='https://img.freepik.com/free-vector/young-businesswoman-holding-tablet-cartoon-illustration_56104-510.jpg?w=900&t=st=1664639506~exp=1664640106~hmac=5d3d08af9079845ed9919cc1de967cfd2de69a4ce845bc646a9b72a281377bab',
                         title='我是品牌部!',
                         text='品牌部雲端連結',
                         actions=[
@@ -102,6 +102,36 @@ def handle_message(event):
                             URIAction(
                                 label='連結在這邊',
                                 uri='https://drive.google.com/drive/folders/1RJm3tqbAMfLisgeaTq8qoHFVdUU708CR?usp=sharing'
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://img.freepik.com/free-vector/variety-time-objects-man-landing-page_52683-23310.jpg?w=740&t=st=1664639567~exp=1664640167~hmac=e9bb9423892fb86d9646dfa728a3a585bc6d6dcf9e3299d3f7c8ae23603eac2d',
+                        title='總時程表',
+                        text='時間照過來!',
+                        actions=[
+                            MessageAction(
+                                label='點我試試?',
+                                text='幫你搔癢'
+                            ),
+                            URIAction(
+                                label='看時程表!',
+                                uri='https://docs.google.com/spreadsheets/d/1KXePCMR5KiKNJe6mzjcFDUih60wwhzm_3v2Ack9YbGU/edit?usp=sharing'
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://img.freepik.com/free-vector/office-table-top-view-business-flat-web-infographic-concept-staff-around-table-report-analytics-working-tablet-laptop-empty-background-brainstorm-report-planning-creative-people-collection_126523-1374.jpg?w=740&t=st=1664638798~exp=1664639398~hmac=2f2acb743def149cc6ba47ea5d334b6ed56d284067a3bf2acb36f2ccc71a3645',
+                        title='開會去!',
+                        text='第一個人要用官帳喔!',
+                        actions=[
+                            MessageAction(
+                                label='點我試試?',
+                                text='幫你搔癢'
+                            ),
+                            URIAction(
+                                label='連結在這邊',
+                                uri='https://meet.google.com/den-yyxq-htq'
                             )
                         ]
                     )
@@ -117,6 +147,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
     elif re.match('送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
+    elif re.match('幫你搔癢',message):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("有事不趕快做?!  快去吧~"))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage("有何貴幹呢? 請按1~"))
 
