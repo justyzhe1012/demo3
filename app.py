d8dc071372fd94f1ad407c74ecc94d3a424e6669
justyzhe1012 <114711290+justyzhe1012@users.noreply.github.com>
2022-10-02 00:01:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('Lz3DQQxDHAndFnJJdQko+TlzV2ylapoCUgp12mtaCeRlTfJieBKwUEl57v3E5ZTVnCiVxs/HWhqk7t8SDG1q+giRdcul9RBeN2AutidC2j0gbOSnNtR2H3b2EcQWXjy2LZWYz7p0SoLlEFHNLz11egdB04t89/1O/w1cDnyilFU=')
  # 必須放上自己的Channel Secret
 handler = WebhookHandler('1994241049acb267fc341cbbadefdbca')
-line_bot_api.push_message('Ub08a25b46cc48ec5f4be8ef499820a4f', TextSendMessage(text='打入「1」開始'))
+line_bot_api.push_message('Ub08a25b46cc48ec5f4be8ef499820a4f', TextSendMessage(text='打入「k」開始'))
 
  # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('stp' ,message):
+    if re.match('k' ,message):
         carousel_template_message = TemplateSendMessage(
             alt_text='我是幹部我驕傲!',
             template=CarouselTemplate(
@@ -52,7 +52,7 @@ def handle_message(event):
                         actions=[
                             MessageAction(
                                 label='累了可以點我',
-                                text='送你一句話:「天空其實一直晴朗，只要你別一直盯著烏雲不放!」'
+                                text='心理冒出一句話:「天空其實一直晴朗，只要你別一直盯著烏雲不放!」'
                             ),
                             URIAction(
                                 label='連結在這邊',
@@ -67,7 +67,7 @@ def handle_message(event):
                         actions=[
                             MessageAction(
                                 label='累了可以點我',
-                                text='送你一句話:「只要你願意為自己努力，世界會給你驚喜!」'
+                                text='心理冒出一句話:「只要你願意為自己努力，世界會給你驚喜!」'
                             ),
                             URIAction(
                                 label='連結在這邊',
@@ -82,7 +82,7 @@ def handle_message(event):
                         actions=[
                             MessageAction(
                                 label='累了可以點我',
-                                text='送你一句話:「生命總有挫折，但那不是盡頭，只是在提醒你該堅持，還是該轉變!」'
+                                text='心理冒出一句話:「生命總有挫折，但那不是盡頭，只是在提醒你該堅持，還是該轉變!」'
                             ),
                             URIAction(
                                 label='連結在這邊',
@@ -97,7 +97,7 @@ def handle_message(event):
                         actions=[
                             MessageAction(
                                 label='累了可以點我',
-                                text='送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」'
+                                text='心理冒出一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」'
                             ),
                             URIAction(
                                 label='連結在這邊',
@@ -139,13 +139,13 @@ def handle_message(event):
             )
         )
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
-    elif re.match('送你一句話:「天空其實一直晴朗，只要你別一直盯著烏雲不放!」',message):
+    elif re.match('心理冒出一句話:「天空其實一直晴朗，只要你別一直盯著烏雲不放!」',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
-    elif re.match('送你一句話:「只要你願意為自己努力，世界會給你驚喜!」',message):
+    elif re.match('心理冒出一句話:「只要你願意為自己努力，世界會給你驚喜!」',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
-    elif re.match('送你一句話:「生命總有挫折，但那不是盡頭，只是在提醒你該堅持，還是該轉變!」',message):
+    elif re.match('心理冒出一句話:「生命總有挫折，但那不是盡頭，只是在提醒你該堅持，還是該轉變!」',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
-    elif re.match('送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」',message):
+    elif re.match('心理冒出一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」送你一句話:「有一種溫柔溫暖的人，即使不知道方法，仍會努力想讓別人幸福!」',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("加油啦!"))
     elif re.match('幫你搔癢',message):
         line_bot_api.reply_message(event.reply_token, TextSendMessage("有事不趕快做?!  快去吧~"))
